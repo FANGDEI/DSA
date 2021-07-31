b9a7bba3c93931efb784eb0c82b2598c65c6f7e6
FANG <1639579565@qq.com>
2021-07-31 12:26:20 +0800

    DP practice

--- QuestionSet/Luogu/DP/p1064.cpp
+++ QuestionSet/Luogu/DP/p1064.cpp
@@ -1,10 +1,65 @@
 //金明的预算方案
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+// int n, m;
+// int f[65][32000+5], t[65][3], v[65], w[65], k, T[65][3];
+// int main() {
+//     cin >> n >> m;
+//     for (int i=1; i<=m; i++) {
+//         int v1, q, p;
+//         cin >> v1 >> q >> p;
+//         if (p == 0) {
+//             w[i] = v1;
+//             v[i] = v1 * q;
+//         } else {
+//             t[p][0]++;
+//             t[p][t[p][0]] = v1;
+//             T[p][t[p][0]] = v1 * q;
+//         }
+//     }
+//     for (int i=1; i<=m; i++) {
+//         for (int j=0; j<=n; j++) {
+//             f[i][j] = f[i-1][j];
+//             if (j>=w[i]) f[i][j] = max(f[i][j], f[i-1][j-w[i]]+v[i]);
+//             if (j>=w[i]+t[i][1]) f[i][j] = max(f[i][j], f[i-1][j-w[i]-t[i][1]]+v[i]+T[i][1]);
+//             if (j>=w[i]+t[i][2]) f[i][j] = max(f[i][j], f[i-1][j-w[i]-t[i][2]]+v[i]+T[i][2]);
+//             if (j>=w[i]+t[i][2]+t[i][1]) f[i][j] = max(f[i][j], f[i-1][j-w[i]-t[i][2]-t[i][1]]+v[i]+T[i][1]+T[i][2]);
+//         }
+//     }
+//     cout << f[m][n] << endl;
+//     return 0;
+// }
+
+/* 优化 */
 #include <iostream>
 #include <algorithm>
 using namespace std;
 int n, m;
-int f[32010], a[32010][3], v[32010];
+int f[32000+5], t[65][3], v[65], w[65], k, T[65][3];
 int main() {
-
+    cin >> n >> m;
+    for (int i=1; i<=m; i++) {
+        int v1, q, p;
+        cin >> v1 >> q >> p;
+        if (p == 0) {
+            w[i] = v1;
+            v[i] = v1 * q;
+        } else {
+            t[p][0]++;
+            t[p][t[p][0]] = v1;
+            T[p][t[p][0]] = v1 * q;
+        }
+    }
+    for (int i=1; i<=m; i++) {
+        for (int j=n; j>=w[i]; j--) {
+            // f[i][j] = f[i-1][j];
+            /*if (j>=w[i]) */f[j] = max(f[j], f[j-w[i]]+v[i]);
+            if (j>=w[i]+t[i][1]) f[j] = max(f[j], f[j-w[i]-t[i][1]]+v[i]+T[i][1]);
+            if (j>=w[i]+t[i][2]) f[j] = max(f[j], f[j-w[i]-t[i][2]]+v[i]+T[i][2]);
+            if (j>=w[i]+t[i][2]+t[i][1]) f[j] = max(f[j], f[j-w[i]-t[i][2]-t[i][1]]+v[i]+T[i][1]+T[i][2]);
+        }
+    }
+    cout << f[n] << endl;
     return 0;
 }
--- QuestionSet/Luogu/DP/p1439.cpp
+++ QuestionSet/Luogu/DP/p1439.cpp
@@ -1,4 +1,19 @@
 //最长公共子序列 50
+/*
+摘自[https://www.luogu.com.cn/blog/blue/solution-p1439]
+关于为什么可以转化成LIS问题，这里提供一个解释。
+A:3 2 1 4 5
+B:1 2 3 4 5
+我们不妨给它们重新标个号：把3标成a,把2标成b，把1标成c……于是变成：
+A: a b c d e
+B: c b a d e
+这样标号之后，LCS长度显然不会改变。但是出现了一个性质：
+两个序列的子序列，一定是A的子序列。而A本身就是单调递增的。
+因此这个子序列是单调递增的。
+换句话说，只要这个子序列在B中单调递增，它就是A的子序列。
+哪个最长呢？当然是B的LIS最长。
+自此完成转化。
+*/
 #include <iostream>
 #include <algorithm>
 using namespace std;
