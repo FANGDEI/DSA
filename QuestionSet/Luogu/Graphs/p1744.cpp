a4d0fec97c4d873649e288ae061b5c8462dd90c3
彭芳洁 <1639579565@qq.com>
2021-08-07 20:08:46 +0800

    SPFA algorithm

---
+++ QuestionSet/Luogu/Graphs/p1744.cpp
@@ -0,0 +1,67 @@
+//P1744 采购特价商品
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+struct Edge {
+    int to;
+    double w;
+    int next;
+} edge[1005 * 2];
+struct Point {
+    int x, y;
+} point[1005 * 2];
+int head[105 * 2], cnt;
+queue<int> q;
+int n, m;
+void add(int u, int v, double w) {
+    edge[++cnt].to = v;
+    edge[cnt].w = w;
+    edge[cnt].next = head[u];
+    head[u] = cnt;
+}
+double dis[105];
+bool vis[105];
+int main() {
+    memset(head, -1, sizeof(head));
+    cin >> n;
+    for (int i=1; i<=n; i++) {
+        int x, y;
+        cin >> x >> y;
+        point[i].x = x, point[i].y = y;
+    }
+    cin >> m;
+    for (int i=1; i<=m; i++) {
+        int u, v;
+        cin >> u >> v;
+        double w = sqrt((point[u].x - point[v].x) * (point[u].x - point[v].x) + 
+        (point[u].y - point[v].y) * (point[u].y - point[v].y));
+        add(u, v, w);
+        add(v, u, w);
+    }
+    int s, t;
+    cin >> s >> t;
+    for (int i=1; i<=n; i++) dis[i] = 0x3f3f3f3f + 0.1;
+    dis[s] = 0;
+    q.push(s);
+    vis[s] = true;
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        vis[x] = false;
+        for (int i=head[x]; ~i; i=edge[i].next) {
+            if (dis[edge[i].to] > dis[x] + edge[i].w) {
+                dis[edge[i].to] = dis[x] + edge[i].w;
+                if (!vis[edge[i].to]) {
+                    q.push(edge[i].to);
+                    vis[edge[i].to] = true;
+                }
+            }
+        }
+    }
+    printf("%.2lf", dis[t]);
+    return 0;
+}
