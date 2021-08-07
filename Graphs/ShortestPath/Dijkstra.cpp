af1e816e6f3663b9a29ca5d2efaeda47b9d38dd7
彭芳洁 <1639579565@qq.com>
2021-08-07 12:19:34 +0800

    Dijkstra

---
+++ Graphs/ShortestPath/Dijkstra.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int N = 1e4 + 5;
+struct Edge {
+    int to;
+    int w;
+    int next;
+} edge[N];
+int head[N], cnt;
+int n, m, s;
+int main() {
+    
+    return 0;
+}
