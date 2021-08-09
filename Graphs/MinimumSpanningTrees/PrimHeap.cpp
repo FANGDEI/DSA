f32f7bda73c9ef8b5da680ea8ef8fbcd36b58586
FANG <1639579565@qq.com>
2021-08-09 19:19:21 +0800

    朴素Prim 堆优化Prim

--- Graphs/MinimumSpanningTrees/PrimHeap.cpp
+++ Graphs/MinimumSpanningTrees/PrimHeap.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <queue>
 using namespace std;
-#define INF 0x3f3f3f3f
+#define INF 0x4f4f4f4f
 const int N = 5005;
 const int M = 2e5 + 5;
 struct Edge {
@@ -14,6 +15,7 @@ int head[N << 1], cnt;
 int n, m, dis[N << 1];
 long long res;
 bool vis[N << 1];
+priority_queue<pair<int, int> > q;
 void add(int u, int v, int w) {
     cnt++;
     edge[cnt].to = v;
@@ -32,22 +34,22 @@ int main() {
     }
     memset(dis, INF, sizeof(dis));
     dis[1] = 0;
-    for (int i=1; i<=n; i++) {
-        int m = INF, x;
-        for (int j=1; j<=n; j++) {
-            if (!vis[j] && dis[j] < m) {
-                m = dis[j];
-                x = j;
+    q.push(make_pair(0, 1));
+    while (!q.empty()) {
+        int x = q.top().second;
+        int y = -q.top().first;
+        q.pop();
+        if (vis[x]) continue; // 懒惰删除
+        vis[x] = true;
+        res += y;
+        for (int i=head[x]; ~i; i=edge[i].next) {
+            if (dis[edge[i].to] > edge[i].w) {
+                dis[edge[i].to] = edge[i].w;
+                q.push(make_pair(-dis[edge[i].to], edge[i].to)); // priority_queue默认大根堆
             }
         }
-        vis[x] = true;
-        res += m;
-        for (int j=head[x]; ~j; j=edge[j].next) {
-            // dis[edge[j].to] = min(dis[edge[j].to], edge[j].w);
-            if (dis[edge[j].to] > edge[j].w) dis[edge[j].to] = edge[j].w;
-        }
     }
-    if (res >= INF) cout << "orz" << endl;
-    else cout << res << endl;
+    for (int i=1; i<=n; i++) if (dis[i] == INF) { cout << "orz" << endl; return 0;}
+    cout << res << endl;
     return 0;
 }
