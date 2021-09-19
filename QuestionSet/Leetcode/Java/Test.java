cc10ab7e3dc41bed376d85706f0d6c6fb4cc0817
彭芳洁 <1639579565@qq.com>
2021-09-19 22:07:48 +0800

    Leetcode

---
+++ QuestionSet/Leetcode/Java/Test.java
@@ -0,0 +1,12 @@
+/*
+ * @Author: FANG
+ * @Date: 2021-09-19 22:07:05
+ * @LastEditTime: 2021-09-19 22:07:37
+ * @Description: 
+ * @FilePath: \DSA\QuestionSet\Leetcode\Java\test.java
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
