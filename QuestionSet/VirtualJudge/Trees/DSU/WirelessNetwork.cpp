853478bd700f272661dfb3860b24b69b70024c15
彭芳洁 <1639579565@qq.com>
2021-10-26 20:46:12 +0800

    Delete something

--- QuestionSet/VirtualJudge/Trees/DSU/WirelessNetwork.cpp
+++ QuestionSet/VirtualJudge/Trees/DSU/WirelessNetwork.cpp
@@ -1,80 +1,51 @@
 /*
  * @Author: FANG
  * @Date: 2021-08-17 16:44:19
- * @LastEditTime: 2021-08-17 17:09:23
- * @Description: https://vjudge.ppsucxtt.cn/problem/POJ-2236
- * @FilePath: \DSA\QuestionSet\VirtualJudge\Trees\DSU\WirelessNetwork.cpp
  */
 #include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <string>
 #include <cmath>
+#include <cstdio>
 #include <algorithm>
 using namespace std;
-
-typedef struct{
-  int x,y;
-  int index;
-}pos;
-
-int pre[1005];
-pos p[1005];
-pos repaired[1005];
-
-void init(){
-   for(int i=1;i<=1000;i++)
-      pre[i]=i;
+struct Point {
+   int x, y;
+   int index;
+} p[1005];
+int repaired[1005], cnt; // The index of the repaired computer and the nums of the repaired computer
+int fa[1005];
+double getDistance(Point a, Point b) {
+   return sqrt(((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y)) * 1.0);
 }
-
-int root_find(int x){
-   return pre[x]==x?x:pre[x]=root_find(pre[x]);
+int find(int x) {
+   return x == fa[x] ? x : fa[x] = find(fa[x]);
 }
-
-void join(int x,int y){
-  int rx=root_find(x);
-  int ry=root_find(y);
-  if(rx!=ry)
-    pre[rx]=ry;
-}
-
-double dis(pos a,pos b){
-   return sqrt((double)(a.x-b.x)*(double)(a.x-b.x)+(double)(a.y-b.y)*(double)(a.y-b.y));
-}
-
-int main()
-{
-    init();
-    int N,D;
-    scanf("%d%d",&N,&D);
-    for(int i=1;i<=N;i++)
-        scanf(" %d %d",&p[i].x,&p[i].y);
-    int cnt=0;
-    char type;
-    while(~scanf(" %c",&type)){
-       if(type=='O') {
-          int temp;
-          scanf("%d",&temp);
-          repaired[cnt].x=p[temp].x;
-          repaired[cnt].y=p[temp].y;
-          repaired[cnt].index=temp;
-          for(int i=0;i<cnt;i++){
-              if(dis(repaired[i],p[temp])<=D){
-
-                  join(repaired[i].index,temp);
-              }
-          }
-          cnt++;
-       }
-       else{
-         int a,b;
-         scanf(" %d %d",&a,&b);
-         if(root_find(a)==root_find(b))
-              printf("SUCCESS\n");
-         else
-              printf("FAIL\n");
-       }
-
-    }
-    return 0;
+int main() {
+   for (int i = 1; i <= 1005; i++) fa[i] = i;
+   int n, d;
+   cin >> n >> d;
+   for (int i = 1; i <= n; i++) {
+      cin >> p[i].x >> p[i].y;
+      p[i].index = i;
+   }
+   char ch;
+   while (scanf(" %c", &ch) != EOF) {
+      if (ch == 'S') {
+         int a, b;
+         cin >> a >> b;
+         if (find(a) == find(b)) {
+            cout << "SUCCESS" << endl;
+         } else cout << "FAIL" << endl;
+      } else {
+         int t;
+         cin >> t;
+         for (int i = 0; i < cnt; i++) {
+            if (getDistance(p[t], p[repaired[i]]) <= d) {
+               // fa[t] = fa[repaired[i]];
+               fa[find(t)] = find(repaired[i]);
+            }
+         }
+         repaired[cnt++] = p[t].index;
+      }
+   }
+   return 0;
 }
